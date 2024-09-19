bfa14c25ca2caa59cbffc432c7dc23444d27aa9c
JuanJoseBotero <jjboterog@eafit.edu.co>
2024-09-19 10:32:38 -0500

    Cambios en gitignore y movie_descriptions

--- movie_descriptions.py
+++ movie_descriptions.py
@@ -57,7 +57,7 @@ print(response)
 # Podemos iterar sobre todas las películas para generar la descripción. Dado que esto 
 #puede tomar bastante tiempo, el archivo con las descripciones para todas las películas es movie_descriptions.json
 
-for i in range(len(movies)):
+""" for i in range(len(movies)):
     prompt =  f"{instruction} Has una descripción de la película {movies[i]['title']}"
     response = get_completion(prompt)
     movies[i]['description'] = response 
@@ -79,4 +79,4 @@ file_path = "movie_descriptions.json"
 with open(file_path, 'w') as json_file:
     json.dump(movies, json_file, indent=4)  # The 'indent' parameter is optional for pretty formatting
 
-print(f"Data saved to {file_path}")
+print(f"Data saved to {file_path}") """
